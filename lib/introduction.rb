7a63925d4d881e528f1da1e81f94263b20dc942c
Tyler Carroll <tylercarroll1611@gmail.com>
2019-09-15 20:13:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -19,10 +19,10 @@ end
 
 
 def introduction_with_language_optional(name, language = "Ruby")
-  puts ""
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
-
+#introduction_with_language_optional("Tyler")
 
 
 
